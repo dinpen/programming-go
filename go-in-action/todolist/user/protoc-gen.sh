8809845d1b42d0cb53ecbb1bef27530785566e66
ding.peng <ding.peng@email.cn>
2023-02-28 22:15:00 +0800

    .

--- go-in-action/todolist/user/protoc-gen.sh
+++ go-in-action/todolist/user/protoc-gen.sh
@@ -1,1 +1,5 @@
-protoc -I internal/service/pb internal/service/pb/*.proto --go_out=. --go-grpc_out=.
+# protoc -I internal/service/pb internal/service/pb/*.proto --go_out=. --go-grpc_out=.
+
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    internal/service/pb/*.proto
